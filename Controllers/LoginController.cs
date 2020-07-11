1eb7bf32ae1ee448f9b745cdabf859bf0e3b078d
AlfMatute <alf.matute@gmail.com>
2020-07-10 20:33:21 -0600

    Login Views, controllers, and models added

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,96 @@
+﻿using CollectionTrackerMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Management;
+using System.Web.Mvc;
+
+namespace CollectionTrackerMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        string ErrorTitle = "Login - Error";
+        string ErrorPart = "An error occurred when trying to Login: ";
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(LoginViewModel model)
+        {
+            try
+            {
+                using(var client = new HttpClient())
+                {
+                    model.Register = true;
+                    client.BaseAddress = new Uri(Properties.Settings.Default.APISetting);
+                    var postJob = client.PostAsJsonAsync<LoginViewModel>("Login", model);
+                    postJob.Wait();
+
+                    var postResult = postJob.Result;
+                    if(postResult.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        var readError = postResult.Content.ReadAsStringAsync();
+                        readError.Wait();
+                        ModelState.AddModelError(String.Empty, readError.Result);
+                    }
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Title = ErrorTitle;
+                ViewBag.ErrorMessage = ErrorPart + ex.Message;
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    model.Register = false;
+                    client.BaseAddress = new Uri(Properties.Settings.Default.APISetting);
+                    var getJob = client.PostAsJsonAsync<LoginViewModel>("Login", model);
+                    getJob.Wait();
+
+                    var result = getJob.Result;
+                    if(result.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        var readError = result.Content.ReadAsStringAsync();
+                        readError.Wait();
+                        ModelState.AddModelError(String.Empty, readError.Result);
+                        return View(model);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                ViewBag.Title = ErrorTitle;
+                ViewBag.ErrorMessage = ErrorPart + ex.Message;
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollectionTrackerMVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember Me?")]
+        public bool RememberMe { get; set; }
+
+        public CollectionUser CollectionUser { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool Register { get; set; }
+    }
+}
